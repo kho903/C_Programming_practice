65ca3424d8eea10993445c08a980aaf92e817e02
kho903 <gmldnr2222@naver.com>
2021-02-07 12:33:58 +0900

    4장 연산자 기본(2)

--- 04 연산자 기본/Operator/Operator.c
+++ 04 연산자 기본/Operator/Operator.c
@@ -19,10 +19,22 @@ int main(void)
 	float avg_int = (first_int + second_int) / 2.0;
 	printf("AVG : %f", avg_int);
 	return 0;
-	*/
+	
 
 	// 초를 '시:분:초"로 변환하는 프로그램
 	int sec = 0;
 	scanf_s("%d", &sec);
 	printf("%d초는 %02d시간 %02d분 %02d초 입니다.", sec, sec/3600, (sec%3600)/60, (sec%3600)%60);
+	*/
+
+	// 세 정수의 누적 및 출력
+	int nInput, nTotal;
+
+	nTotal = 0;
+	for (int i = 0; i < 3; i++) {
+		scanf_s("%d", &nInput);
+		nTotal += nInput;
+	}
+
+	printf("Total : %d\n", nTotal);
 }
